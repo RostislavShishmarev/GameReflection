ad99ece04de9827d1460e1cad5b43ca5f1f9941e
RostislavShishmarev <shishmarev.r@mail.ru>
2021-12-15 15:48:43 +0300

    Добавлены папки для изображений и БД, начата работа с кодом

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame as pg
 import pygame.draw as dr
+from datetime import time as Time
 
 
 class Game:
-    pass
+    def __init__(self, csv_model_name, score, time):
+        pass
+
+    def run(self):
+        pass
+
+    def render(self):
+        pass
 
 
 class MainWindow:
@@ -11,4 +19,6 @@ class MainWindow:
 
 
 if __name__ == '__main__':
-    pass
+    time = Time(minute=0, second=0)
+    window = Game('DataBases/Level1_StartModel.csv', 0, time)
+    window.run()
