b78c6a580f9d341484e0cc0b2d6003fb2fb56296
RostislavShishmarev <shishmarev.r@mail.ru>
2021-12-18 16:18:41 +0300

    Созданы изображения курсора и платформы, добавлен класс платформы

--- main.py
+++ main.py
@@ -1,12 +1,76 @@
 import pygame as pg
 import pygame.draw as dr
+import pygame.transform as tr
+import pygame.sprite as spr
 import csv
 from datetime import datetime as DateTime
 from datetime import timedelta as TimeDelta
+import os
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('Images', name)
+    try:
+        image = pg.image.load(fullname)
+    except pg.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        image = image.convert()
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 def do_nothing():
     pass
+
+
+def get_width(surface, height):
+    return round(surface.get_size()[0] * (height / surface.get_size()[1]))
+
+
+class Platform(spr.Sprite):
+    def __init__(self, parent, group):
+        super().__init__()
+        self.parent = parent
+        image = load_image("Platform.png", -1)
+        self.h = 80
+        self.w = get_width(image, self.h)
+        self.image = tr.scale(image, (self.w, self.h))
+        self.rect = self.image.get_rect()
+        self.rect.x =  self.parent.w // 2 - self.w // 2
+        self.rect.y = 650
+        self.selected_delta_x = 0
+        self.add(group)
+
+    def update(self, delta_x):
+        x = self.rect.x + delta_x - self.selected_delta_x
+        self.rect.x = x if x >= 0 and x <= self.parent.w - self.w\
+            else self.rect.x
+
+    def set_platform_size(self, size='middle'):
+        name = {'short': "Short_platform.png", 'long': "Long_platform.png",
+                'middle': "Platform.png"}.get(size, None)
+        old_x = self.rect.x
+        if name is None:
+            print('Некорректный размер.')
+            return
+        image = load_image(name, -1)
+        self.h = 80
+        self.w = get_width(image, self.h)
+        self.image = tr.scale(image, (self.w, self.h))
+        self.rect = self.image.get_rect()
+        self.rect.x = min(old_x, self.parent.w - self.rect.w)
+        self.rect.y = 650
+
+    def set_select(self, select, pos=None):
+        pos = (self.rect.x, self.rect.y) if pos is None else pos
+        self.selected_delta_x = pos[0] - self.rect.x if select else 0
 
 
 class Button:
@@ -21,13 +85,19 @@ class Button:
         self.current_text = self.text
         self.font_size = font_size
         self.main_color = main_color
+        self.light_main_color = main_color
         self.back_color = back_color
+        self.num_of_change = 0
         self.function = slot
 
     def slot(self):
         # Декорация переданной функции:
         self.current_text = self.text if self.current_text == self.text2\
             else self.text2
+        self.light_main_color = pg.Color(min(self.main_color.r + 90, 255),
+                                         min(self.main_color.g + 90, 255),
+                                         min(self.main_color.b + 90, 255))
+        self.num_of_change = 10
 
         self.function()
 
@@ -35,12 +105,16 @@ class Button:
         screen = self.parent.screen
         dr.rect(screen, self.back_color,
                 (self.x, self.y, self.w, self.h))
-        dr.rect(screen, self.main_color,
+        dr.rect(screen, self.light_main_color,
                 (self.x, self.y, self.w, self.h), width=2)
         font = pg.font.Font(None, self.font_size)
-        text = font.render(self.current_text, True, self.main_color)
+        text = font.render(self.current_text, True, self.light_main_color)
         screen.blit(text, (self.x + self.w // 2 - text.get_width() // 2,
                            self.y + self.h // 2 - text.get_height() // 2))
+        if self.num_of_change == 0:
+            self.light_main_color = self.main_color
+        else:
+            self.num_of_change -= 1
 
     def __contains__(self, item):
         return item[0] in range(self.x, self.x1) and\
@@ -107,9 +181,11 @@ class Game:
         self.block_width = 90
         self.block_height = 50
 
+        # Флаги:
         self.pause = False
         self.running = True
         self.start = True
+        self.platform_selected = False
 
         # Создаём виджеты:
         self.buttons = [Button(self, 20, self.h - 70, 350, 50,
@@ -125,6 +201,8 @@ class Game:
                                      'Жизни', str(self.lifes), image=0),
                          TextDisplay(self, self.w - 320, 20, 300, 100,
                                      'Время', self.time.strftime('%M:%S'))]
+        self.all_sprites = spr.Group()
+        self.cursor_group = spr.Group()
 
         # Открываем модель расположения блоков:
         with open(self.mod_name, encoding='utf8') as model:
@@ -143,9 +221,17 @@ class Game:
         self.screen = pg.display.set_mode(self.size)
         self.screen.fill(background_color)
         pg.time.set_timer(SECOND, 1000)
+        pg.mouse.set_visible(False)
+
+        # Создаём спрайты:
+        self.platform = Platform(self, self.all_sprites)
+        self.cursor = spr.Sprite(self.cursor_group)
+        self.cursor.image = load_image("cursor.png")
+        self.cursor.rect = self.cursor.image.get_rect()
 
         # Основной цикл игры:
         while self.running:
+            # Обработка событий:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
@@ -153,6 +239,9 @@ class Game:
                     for but in self.buttons:
                         if event.pos in but:
                             but.slot()
+                    if self.platform.rect.collidepoint(event.pos):
+                        self.platform_selected = True
+                        self.platform.set_select(True, event.pos)
                 if event.type == SECOND:
                     if not self.pause:
                         self.time += TimeDelta(seconds=1)
@@ -163,12 +252,29 @@ class Game:
                         self.buttons[0].slot()  # Сохранить
                     if event.key == pg.K_HOME and (event.mod & pg.KMOD_CTRL):
                         self.buttons[2].slot()  # Выход
+                if event.type == pg.MOUSEMOTION:
+                    self.cursor.rect.topleft = event.pos
+                    if self.platform_selected:
+                        self.platform.update(event.pos[0] -\
+                                             self.platform.rect.x)
+                if event.type == pg.MOUSEBUTTONUP:
+                    self.platform_selected = False
+                    self.platform.set_select(False)
+            if pg.key.get_pressed()[pg.K_LEFT]:
+                if not pg.key.get_mods() & pg.KMOD_SHIFT:
+                    self.platform.update(-10)
+            if pg.key.get_pressed()[pg.K_RIGHT]:
+                if not pg.key.get_mods() & pg.KMOD_SHIFT:
+                    self.platform.update(10)
 
             # Отрисовка элементов:
             self.screen.fill(background_color)
             self.render()
             for el in self.buttons + self.displays:
                 el.render()
+            self.all_sprites.draw(self.screen)
+            if pg.mouse.get_focused():
+                self.cursor_group.draw(self.screen)
 
             # Обновление элементов дисплеев:
             for i, el in enumerate((str(self.score), str(self.lifes),
