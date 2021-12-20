c2ef227402e6216fcf4b62d8bb44d901900b0423
Sofia096 <sofiacat20062508@gmail.com>
2021-12-20 19:59:37 +0300

    Commit1

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame
-import pygame.draw
+import sys
+from pygame.locals import *
+import pygame as pg
+import pygame.image as img
+import pygame.draw as dr
+import pygame.transform as tr
+import pygame.sprite as spr
+import csv
+from datetime import datetime as DateTime
+from datetime import timedelta as TimeDelta
+import os
 
 
 class Game:
@@ -8,32 +18,54 @@ class Game:
 
 
 class MainWindow:
-    pass
+    def __init__(self):
+        pygame.init()
+        #Экран
+        size = width, height = 1240, 700
+        screen = pygame.display.set_mode(size, HWSURFACE | DOUBLEBUF | RESIZABLE)
+        #Иконка
+        pg.display.set_caption('Отражение')
+        pg.display.set_icon(self.load_image('Reflection_logo_2.png'))
+        #Флаг
+        pygame.mouse.set_visible(False)
+        #Цикл
+        while pygame.event.wait().type != pygame.QUIT:
+            #Фон
+            win = pygame.display.get_surface()
+            x = win.get_width()
+            y = win.get_height()
+            fone1 = tr.scale(self.load_image('Fone.png'), (x, y))
+            # Курсор
+            image = self.load_image('Cursor.png')
+            cursor_img_rect = image.get_rect()
+            cursor_img_rect.center = pygame.mouse.get_pos()
+            screen.blit(image, cursor_img_rect)
+            #Фон и кнопки
+            pygame.display.flip()
+            screen.blit(fone1, (0, 0))
+            self.draw(screen, x - 140, y - 55)
+        pygame.quit()
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('Images', name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        return image
+
+    def draw(self, screen, x, y):
+        font = pygame.font.Font(None, 50)
+        text = font.render("Выход", True, (100, 255, 100))
+        text_x = x
+        text_y = y
+        text_w = text.get_width()
+        text_h = text.get_height()
+        pygame.draw.rect(screen, (100, 255, 100), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
+        screen.blit(text, (text_x, text_y))
+
 
 
 if __name__ == '__main__':
-    pygame.init()
-    res = (720, 720)
-    screen = pygame.display.set_mode(res)
-    color = (255, 255, 255)
-    color_light = (170, 170, 170)
-    color_dark = (100, 100, 100)
-    width = screen.get_width()
-    height = screen.get_height()
-    smallfont = pygame.font.SysFont('Corbel', 35)
-    text = smallfont.render('quit', True, color)
-    while True:
-        for ev in pygame.event.get():
-            if ev.type == pygame.QUIT:
-                pygame.quit()
-            if ev.type == pygame.MOUSEBUTTONDOWN:
-                if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
-                    pygame.quit()
-        screen.fill((60, 25, 60))
-        mouse = pygame.mouse.get_pos()
-        if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
-            pygame.draw.rect(screen, color_light, [width / 2, height / 2, 140, 40])
-        else:
-            pygame.draw.rect(screen, color_dark, [width / 2, height / 2, 140, 40])
-        screen.blit(text, (width / 2 + 50, height / 2))
-        pygame.display.update()
+    Window = MainWindow()
