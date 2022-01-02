8f7204a0fb6cd6c81a0712c499399931901c1a78
RostislavShishmarev <shishmarev.r@mail.ru>
2022-01-02 18:26:59 +0300

    Начат класс окна

---
+++ infowindow.py
@@ -0,0 +1,73 @@
+import pygame as pg
+import pygame.transform as tr
+import pygame.sprite as spr
+
+from functions import load_image, do_nothing, get_width
+
+
+class InfoWindow:
+    def __init__(self):
+        # Задаём атрибуты:
+        self.FPS = 60
+        self.size = (self.w, self.h) = (1210, 820)
+        self.blocks_left = 20
+        self.blocks_top = 140
+        self.field_bottom = 730
+        self.block_width = 90
+        self.block_height = 50
+        self.death_y = 710
+        self.border_w = 20
+
+        self.cursor_group = spr.Group()
+
+        # Флаги:
+        self.running = True
+        self.new_window_after_self = None
+
+    def run(self):
+        pg.init()
+        # Местные переменные и константы:
+        clock = pg.time.Clock()
+
+        # Задаём параметры окну:
+        pg.display.set_caption('Отражение: правила')
+        self.screen = pg.display.set_mode(self.size)
+        im = load_image('Fone.png')
+        fone = tr.scale(im, (get_width(im, self.h), self.h))
+        self.screen.blit(fone, (0, 0))
+        pg.mouse.set_visible(False)
+        pg.display.set_icon(load_image('Reflection_logo.png'))
+
+        # Создаём спрайты:
+        self.cursor = spr.Sprite(self.cursor_group)
+        self.cursor.image = load_image("cursor.png")
+        self.cursor.rect = self.cursor.image.get_rect()
+
+        # Основной цикл игры:
+        while self.running:
+            # Обработка событий:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                if event.type == pg.MOUSEMOTION:
+                    self.cursor.rect.topleft = event.pos
+
+            # Отрисовка элементов:
+            self.screen.blit(fone, (0, 0))
+            if pg.mouse.get_focused():
+                self.cursor_group.draw(self.screen)
+
+            # Обновление элементов:
+            clock.tick(self.FPS)
+            pg.display.flip()
+        pg.quit()
+        if self.new_window_after_self is not None:
+            self.new_window_after_self.run()
+
+    def exit(self):
+        self.running = False
+
+
+if __name__ == '__main__':
+    window = InfoWindow()
+    window.run()
